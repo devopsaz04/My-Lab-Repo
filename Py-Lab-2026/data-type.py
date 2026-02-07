e80474af5a4ccb8a569f12f1f3425bebb2f6ce90
DevOps <devopsaz04@gmail.com>
2026-02-07 12:33:02 +0000

    update file

--- Py-Lab-2026/data-type.py
+++ Py-Lab-2026/data-type.py
@@ -1,7 +1,108 @@
-CodeExpert = 'satish.chikkadi'
-print(CodeExpert)
+# CodeExpert = 'satish.chikkadi'
+# print(CodeExpert)
 
-str1 = "satish"
-str2 = "chikkadi"
-result = str1 + " " + str2
-print(result)
+# str1 = "satish"
+# str2 = "chikkadi"
+# result = str1 + " " + str2
+# print(result)
+
+
+# text = "Python is awesome"
+# length = len(text)
+# print("Length of the string:", length)
+
+# text = "satishchikkadi"
+# length = len(text)
+# print("Lenth of the string", length)
+
+# text = "satishchikkadi"
+# print(text.upper())
+
+
+# text = "satish chikkadi"
+# new_text = text.replace("satish", "sahitya")
+# print("The replaced words:", new_text)
+
+# text = "Python is awesome"
+# new_text = text.replace("awesome", "great")
+# print("Modified text:", new_text)
+
+# text = "satishchikkadi"
+# words = text.split()
+# print("Words:", words)
+
+# text = "   Some spaces around   "
+# stripped_text = text.strip()
+# print("Stripped text:", stripped_text)
+
+
+# text = "satish is CodeExpert!!"
+# substring = "is"
+# if substring in text:
+#     print(substring, "found in the text")
+
+
+# text = "This is AWS EC2 instance"
+# substring = "EC2"
+# if substring in text:
+#     print(substring, "is found")
+
+    # Float variables
+num1 = 5.0
+num2 = 2.5
+
+# Basic Arithmetic
+# result1 = num1 + num2
+# print("Addition:", result1)
+
+# result2 = num1 - num2
+# print("Subtraction:", result2)
+
+# result3 = num1 * num2
+# print("Multiplication:", result3)
+
+# result4 = num1 / num2
+# print("Division:", result4)
+
+# # Rounding
+# result5 = round(3.14159265359, 2)  # Rounds to 2 decimal places
+# print("Rounded:", result5)
+
+# Integer Examples
+
+# 1. Basic integer variables
+# age = 25
+# temperature = -10
+# count = 0
+
+# # 2. Integer arithmetic operations
+# x = 10
+# y = 3
+# print(x + y)      # Addition: 13
+# print(x - y)      # Subtraction: 7
+# print(x * y)      # Multiplication: 30
+# print(x / y)      # Division: 3.333...
+# print(x // y)     # Floor division: 3
+# print(x % y)      # Modulo (remainder): 1
+# print(x ** y)     # Power: 1000
+
+# # 3. Type checking
+# num = 42
+# print(type(num))  # <class 'int'>
+
+# # 4. Converting to int
+# string_num = "100"
+# converted = int(string_num)
+# print(converted)  # 100
+
+# # 5. Integer literals with different bases
+# decimal = 255       # Decimal
+# binary = 0b11111111  # Binary (FF in hex)
+# octal = 0o377        # Octal
+# hexadecimal = 0xFF   # Hexadecimal
+
+# # 6. Large integers (Python has unlimited precision)
+# large_num = 123456789012345678901234567890
+
+# num = "satish"
+# print(type(num))
