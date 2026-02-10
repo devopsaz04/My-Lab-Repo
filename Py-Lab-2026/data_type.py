5208c155870c75307c0301634128a0846d9717f8
DevOps <devopsaz04@gmail.com>
2026-02-10 12:55:45 +0000

    update

--- Py-Lab-2026/data_type.py
+++ Py-Lab-2026/data_type.py
@@ -1,5 +1,44 @@
-def greeting(name):
-    print("Hello, " + name)
+# # def greeting(name):
+# #     print("Hello, " + name)
 
 
-person1 = {"name": "John", "age": 36, "country": "Norway"}
+# # person1 = {"name": "John", "age": 36, "country": "Norway"}
+
+# import json
+
+# x = {
+#     "name": "John",
+#     "age": 30,
+#     "married": True,
+#     "divorced": False,
+#     "children": ("Ann", "Billy"),
+#     "pets": None,
+#     "cars": [{"model": "BMW 230", "mpg": 27.5}, {"model": "Ford Edge", "mpg": 24.1}],
+# }
+
+# print(json.dumps(x, indent=4, sort_keys=True))
+
+# import re
+
+# txt = "The rain in Spain"
+# x = re.search("^The.*Spain$", txt)
+# print(x)
+
+# import camelcase
+
+# c = camelcase.CamelCase()
+# txt = "hello world"
+# print(c.hump(txt))
+
+# print("Enter your name:")
+# x = input()
+# print("Hello, " + x)
+
+import os
+folder = input("Enter folder name: ")
+
+
+files = os.listdir(folder)
+print("Files in " + folder + ":")
+for file in files:
+    print("The file name is:",file)
--- Py-Lab-2026/module.py
+++ Py-Lab-2026/module.py
@@ -1,5 +1,8 @@
-from data_type import greeting, person1
+# from data_type import greeting, person1
 
-print(person1["age"])
+# print(person1["age"])
 
-import datatime
+# import datetime
+
+# x = datetime.datetime(2026, 2, 10)
+# print(x)
